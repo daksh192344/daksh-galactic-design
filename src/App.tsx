46d9fa48285610956df237eef19a0d43accca4f3
daksh192344 <dragonmc1st@gmail.com>
2026-03-07 10:38:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,67 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { AuthProvider } from "@/hooks/useAuth";
+
 import Index from "./pages/Index";
+import Portfolio from "./pages/Portfolio";
+import JoinTeam from "./pages/JoinTeam";
+
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
-import JoinTeam from "./pages/JoinTeam";
+
 import TeamLogin from "./pages/TeamLogin";
 import TeamDashboard from "./pages/TeamDashboard";
-import Portfolio from "./pages/Portfolio";
+
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/join" element={<JoinTeam />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/admin" element={<AdminLogin />} />
-            <Route path="/admin/dashboard" element={<AdminDashboard />} />
-            <Route path="/team/login" element={<TeamLogin />} />
-            <Route path="/team/dashboard" element={<TeamDashboard />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AuthProvider>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+
+        <BrowserRouter>
+
+          <AuthProvider>
+
+            <Routes>
+
+              {/* Main Pages */}
+              <Route path="/" element={<Index />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/join" element={<JoinTeam />} />
+
+              {/* Admin */}
+              <Route path="/admin" element={<AdminLogin />} />
+              <Route path="/admin/dashboard" element={<AdminDashboard />} />
+
+              {/* Team */}
+              <Route path="/team/login" element={<TeamLogin />} />
+              <Route path="/team/dashboard" element={<TeamDashboard />} />
+
+              {/* 404 */}
+              <Route path="*" element={<NotFound />} />
+
+            </Routes>
+
+          </AuthProvider>
+
+        </BrowserRouter>
+
+      </TooltipProvider>
+
+    </QueryClientProvider>
+  );
+}
 
 export default App;
